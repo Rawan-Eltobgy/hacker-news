995e1e0a79ee3f42f463f504f626e2f81aec1b7d
Rawan Eltobgy <rawan.eltobgy@harmonica.life>
2021-06-18 03:19:26 +0200

    feat: Add custom hook to fetch data and draw the graph

---
+++ src/components/dataCard/DataCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './ProfileCard.module.css';
+
+export default function DataCard() {
+  return (
+    <div className={styles.Card}>
+        <div className={styles.Card__row}>
+          <text></text>
+        </div>
+        <div className={styles.Card__row}>
+          <text></text>
+        </div>
+      <div className={styles.Card__summary}>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/fetchDataHooks.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useReducer } from "react";
+
+export const useFetch = (url, storiesNumber = 10) => {
+  const cache = useRef({});
+
+  const initialState = {
+    status: "idle",
+    error: null,
+    data: [],
+    scores: [],
+    descendants: [],
+  };
+
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "FETCHING":
+        return { ...initialState, status: "fetching" };
+      case "FETCHED":
+        return { ...initialState, status: "fetched", data: action.payload };
+      case "FETCH_ERROR":
+        return { ...initialState, status: "error", error: action.payload };
+      case "FETCHED DETAILS":
+        return {
+          ...initialState,
+          status: "fetched Details",
+          data: action.payload.data,
+          scores: action.payload.scores,
+          descendants: action.payload.descendants,
+        };
+      default:
+        return state;
+    }
+  }, initialState);
+
+  const fetchStoriesIds = async()=>{
+        const response = await fetch(url);
+        const data = await response.json();
+        return data
+  }
+
+  useEffect(() => {
+    let cancelRequest = false;
+    if (!url) return;
+
+    const fetchData = async () => {
+      dispatch({ type: "FETCHING" });
+      try {
+        const scores = [],
+          descendantsArr = [];
+        const data = await fetchStoriesIds()
+        // cache.current[url] = data;
+        // if (cancelRequest) return;
+        // dispatch({ type: 'FETCHED', payload: data });
+        await Promise.all([...data.map((async function (item, index) {
+            if (index >= storiesNumber) return;
+            const storyURL =
+              item &&
+              `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`;
+            const response = await fetch(storyURL);
+            const story = await response.json();
+            const { score, descendants } = story;
+            scores.push(Number(score));  
+            descendantsArr.push(Number(descendants));
+          }))])
+        dispatch({
+          type: "FETCHED DETAILS",
+          payload: { data: data, scores: scores, descendants: descendantsArr },
+        });
+      } catch (error) {
+        if (cancelRequest) return;
+        dispatch({ type: "FETCH_ERROR", payload: error.message });
+      }
+    };
+
+    fetchData();
+
+    return function cleanup() {
+      cancelRequest = true;
+    };
+  }, [url, storiesNumber]);
+
+  return state;
+};
